e1051dd6dd3685902ccce248ca4e7bc3e2181e42
coop711 <coop711@icloud.com>
2017-09-18 01:26:22 +0900

    Korea map added

--- temp.R
+++
@@ -1,4 +0,0 @@
-p <- c("ggmap", "rgdal", "rgeos", "maptools", "dplyr", "tidyr", "tmap")
-install.packages(p,
-                 repos = "https://cran.rstudio.com")
-lapply(p, library, character.only = TRUE)
